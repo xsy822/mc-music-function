27a8e3bbf461848b731bdf51af56abe4cb1b1216
xsy822 <2239499647@qq.com>
2021-02-15 22:06:33 +0800

    first commit

---
+++ music.py
@@ -0,0 +1,36 @@
+# 生成程序
+import wujiaoxin as par
+import os
+
+
+class Track:
+    """音轨类:
+        - add:增加一个乐符
+    """
+
+    def __init__(self, funName='test'):
+        self.pos = [0, 0, 0]
+        init(funName)
+
+    def add(self, tone, delay, effect='wujiaoxin'):
+        self.newpos = [self.pos[0], self.pos[1], self.pos[2]]
+        pass
+
+
+def init(funName):
+    """创建基础function
+        - funName:函数名称
+    """
+    url = 'functions\\' + funName + '\\main'
+    if not os.path.exists(url):
+        os.makedirs(url)
+    url = 'functions\\' + funName + '\\init.mc.function'
+    with open(url, 'w') as fp:
+        s = 'scoreboard objectives add ' + funName + ' dummy ' + funName
+        fp.write(s)
+    url = 'functions\\' + funName + '\\main.mc.function'
+    fp = open(url, 'w')
+    fp.close()
+
+
+newTrack = Track('a')
---
+++ wujiaoxin.py
@@ -0,0 +1,30 @@
+# 五角星特效
+import math as m
+
+
+def wujiaoxin(particle, pos, r, fp):
+    """指定地点绘制五角星
+        - particle:粒子名称
+        - pos:三维坐标
+        - r:五角星外接圆半径
+        - fp:文件指针
+    """
+    th1, th2 = 0, 360
+    x, y, x1, y1, x2, y2, xc, yc = 0, 0, 0, 0, 0, 0, 0, 0
+    while th1 <= 360:
+        x = r * m.cos(m.radians(th1))
+        y = r * m.sin(m.radians(th1))
+        xc = r * 3 / 2 * m.cos(m.radians(th1)) + pos[0]
+        yc = r * 3 / 2 * m.sin(m.radians(th1)) + pos[2]
+        s = 'particle %s ~%.2f ~%.2f ~%.2f\n' % (particle, xc, pos[1], yc)
+        fp.write(s)
+        x1 = x + (r / 2) * m.cos(m.radians(th2)) + pos[0]
+        y1 = y + (r / 2) * m.cos(m.radians(th2)) + pos[2]
+        s = 'particle %s ~%.2f ~%.2f ~%.2f\n' % (particle, x1, pos[1], y1)
+        fp.write(s)
+        x2 = x + r / 2 * m.cos(m.radians(th2+180)) + pos[0]
+        y2 = y + r / 2 * m.cos(m.radians(th2+180)) + pos[2]
+        s = 'particle %s ~%.2f ~%.2f ~%.2f\n' % (particle, x2, pos[1], y2)
+        fp.write(s)
+        th1 += 6
+        th2 -= 9
